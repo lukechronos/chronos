1b7b824138973ed44349f4f073509aff9087603d
Cameron Barker <barker.cameron@gmail.com>
2015-11-01 21:42:04 -0500

    hiding carousel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(function(){
+  
+});
+
+$("[data-hover~=show-controls]").hover(
+  function() {
+    $('.carousel-control').show();
+  }, function() {
+    $('.carousel-control').hide();
+  }
+);
