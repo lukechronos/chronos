db6782c1499cb71cb007da069208219e9700db3b
Cameron Barker <barker.cameron@gmail.com>
2015-11-02 21:26:32 -0500

    adding styling for preorder and about

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,22 @@
+// Sticky Nav
+var slideshowY;
+var headerContainer = $(".header-container");
+var body = $("body");
+var headerHeight = headerContainer.height();
+var preorderButton = $(".header-container .preorder");
+
 $(function(){
+  if($("body").hasClass("preorder-body") || $("body").hasClass("about-body")){
+    slideshowY = headerHeight;
+  }else{
+    slideshowY = $('.vertical-slideshow').offset().top;
+  }
+  // Pull down hand in disc section
   $('.pull-down').each(function() {
     $(this).css('margin-top', $(this).parent().height()-$(this).height());
   });
+
+
 });
 
 $("[data-hover~=change-image").hover(
@@ -20,11 +35,6 @@ $("[data-hover~=change-format").hover(
 );
 
 // Sticky Nav
-var slideshowY = $('.vertical-slideshow').offset().top;
-var headerContainer = $(".header-container");
-var body = $("body");
-var headerHeight = headerContainer.height();
-var preorderButton = $(".header-container .preorder");
 function stickyScroll(e) {
   if( window.pageYOffset > slideshowY ) {
     console.log("below fold");
